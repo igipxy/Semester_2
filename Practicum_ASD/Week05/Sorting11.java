d1f51032fa13c779538abf02ef5b5f12ebdbc4b2
iGipxy <gesangrah@gmail.com>
2026-04-07 12:04:50 +0700

    few change

---
+++ Practicum_ASD/Week05/Sorting11.java
@@ -0,0 +1,33 @@
+package Week05;
+
+public class Sorting11 {
+    int[] data;
+    int size;
+
+}
+
+public Sorting11(int[] data) {
+    this.data = data;
+    this.size = data.length;
+
+}
+
+public void bubblesort() {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0;  j < size - i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+
+            }
+        }
+    }
+}
+
+public void print() {
+    for (int num : data) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+}

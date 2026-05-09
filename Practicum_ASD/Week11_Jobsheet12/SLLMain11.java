b047df58dffe2208048eea78a4a9ce8b4bf2dd0e
iGipxy <gesangrah@gmail.com>
2026-05-09 10:20:47 +0700

    sll main

--- Practicum_ASD/Week11_Jobsheet12/SLLMain11.java
+++ Practicum_ASD/Week11_Jobsheet12/SLLMain11.java
@@ -1,27 +1,30 @@
 public class SLLMain11 {
     public static void main(String[] args) {
-        SingleLinkedList11 sll = new SingleLinkedList11();
+    SingleLinkedList11 sll = new SingleLinkedList11();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter NIM: ");
-        String nim = sc.nextLine();
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter Class: ");
-        String kls = sc.nextLine();
-        System.out.print("Enter GPA: ");
-        double gpa = sc.nextDouble();
-        sc.nextLine(); // consume newline
-        Student11 std = new Student11(nim, name, kls, gpa);
-        sll.addLast(std); // or any method you want
+    Student11 std1 = new Student11("001", "Student 1", "TI-1I", 3.89);
+    Student11 std2 = new Student11("002", "Student 2", "TI-1I", 3.45);
+    Student11 std3 = new Student11("003", "Student 3", "TI-1I", 3.20);
+    Student11 std4 = new Student11("004", "Student 4", "TI-1I", 3.00);
 
-        sll.print();
-        sll.addFirst(std4);
-        sll.print();
-        sll.addLast(std1);
-        sll.print();
-        sll.insertAfter(std3, "Student 4");
-        sll.insertAt(2, std2);
-        sll.print();
-    }
+    sll.print();
+    sll.addFirst(std4);
+    sll.print();
+    sll.addLast(std1);
+    sll.print();
+    sll.insertAfter(std3, "Student 4");
+    sll.insertAt(2, std2);
+    sll.print();
+
+    System.out.println("Data at index 1 is:");
+    Student11 data = sll.getData(1);
+    data.print();
+    int idx = sll.indexOf("Student 1");
+    System.out.println("Student 1 is located at index: " + idx);
+    sll.removeFirst();
+    sll.removeLast();
+    sll.print();
+    sll.removeAt(0);
+    sll.print();
+}
 }

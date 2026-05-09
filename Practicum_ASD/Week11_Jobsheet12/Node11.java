2807f07fea29872ce5f0770941be57b7fd597c2a
iGipxy <gesangrah@gmail.com>
2026-05-09 10:02:26 +0700

    make node11

---
+++ Practicum_ASD/Week11_Jobsheet12/Node11.java
@@ -0,0 +1,12 @@
+public class Node11 {
+    Student11 data;
+    Node11 next;
+
+    public Node11() {
+    }
+
+    public Node11(Student11 data, Node11 next) {
+        this.data = data;
+        this.next = next;
+    }
+}

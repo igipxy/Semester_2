3193f415a976d3cc2339f8d7c433b8b57e046258
iGipxy <gesangrah@gmail.com>
2026-05-09 10:08:51 +0700

    make the sll and the main file

---
+++ Practicum_ASD/Week11_Jobsheet12/SLLMain11.java
@@ -0,0 +1,19 @@
+public class SLLMain11 {
+    public static void main(String[] args) {
+        SingleLinkedList11 sll = new SingleLinkedList11();
+
+        Student11 std1 = new Student11("001", "Student 1", "TI-1I", 3.89);
+        Student11 std2 = new Student11("002", "Student 2", "TI-1I", 3.45);
+        Student11 std3 = new Student11("003", "Student 3", "TI-1I", 3.20);
+        Student11 std4 = new Student11("004", "Student 4", "TI-1I", 3.00);
+
+        sll.print();
+        sll.addFirst(std4);
+        sll.print();
+        sll.addLast(std1);
+        sll.print();
+        sll.insertAfter(std3, "Student 4");
+        sll.insertAt(2, std2);
+        sll.print();
+    }
+}

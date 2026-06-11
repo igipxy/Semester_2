ec9e5da2ab39a7150dad5e972f6f34e858dc27da
iGipxy <gesangrah@gmail.com>
2026-06-12 06:03:40 +0700

    added node 11

---
+++ Practicum_ASD/Week14/Node11.java
@@ -0,0 +1,16 @@
+package Practicum_ASD.Week14;
+
+public class Node11 {
+    Student11 data;
+    Node11 left;
+    Node11 right;
+
+    Node11() {
+    }
+
+    Node11(Student11 data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
